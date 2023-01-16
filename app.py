8a01935942436f199b0a245eaa655e62e8301a23
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2023-01-16 15:00:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+#nueva route usando query strings
+@app.route('/new/')
+def query_strings(greeting='hello'):
+    query_val = request.args.get('greeting', greeting)
+    return '<h1> the greeting is: {0} </h1>'.format(query_val)
+
+
+
 if __name__ == '__main__':
     app.run()
 
