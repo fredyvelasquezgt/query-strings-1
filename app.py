e81ca54044fc603e25ca0ad86f19f830d79560c9
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2023-01-16 15:10:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,30 @@ def no_query_strings(name='mina'):
     return '<h1>hello there! {} </h1>'.format(name)
 
 
+# strings
+@app.route('/text/<string:name>')
+def working_with_strings(name):
+    return '<h1> here is a string: ' + name + '</h1>'
+
+
+# numbers
+@app.route('/numbers/<int:num>')
+def working_with_numbers(num):
+    return '<h1> the number you picked is: ' + str(num) + '</h1>' 
+
+
+# add numbers
+@app.route('/add/<int:num1>/<int:num2>')
+def adding_integers(num1, num2):
+    return '<h1> the sum is : {}'.format(num1 + num2) + '</h1>'
+
+
+# floats
+@app.route('/product/<float:num1>/<float:num2>')
+def product_two_numbers(num1, num2):
+    return '<h1> the product is : {}'.format(num1 * num2) + '</h1>'
+
+
 
 if __name__ == '__main__':
     app.run()
