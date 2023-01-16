77de7920b51bc34d16a8914df4b28476cff190c6
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2023-01-16 16:28:01 -0600

    Jinja #2 - Filters

--- app.py
+++ app.py
@@ -80,6 +80,20 @@ def movies_plus():
                            movies=movies_dict,
                            name='Sally')
 
+# JINJA2 - FILTERS
+@app.route('/filters')
+def filter_data():
+    movies_dict = {'autopsy of jane doe': 02.14,
+                   'neon demon': 3.20,
+                   'ghost in a shell': 1.50,
+                   'kong: skull island': 3.50,
+                   'john wick 2': 02.52,
+                   'spiderman - homecoming': 1.48}
+
+    return render_template('filter_data.html',
+                           movies=movies_dict,
+                           name=None,
+                           film='a christmas carol')
 
 
 if __name__ == '__main__':
