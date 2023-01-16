c77229cdf5ee482645481ea00a182f348669edb6
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2023-01-16 16:11:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -66,6 +66,20 @@ def top_movies():
                            movies=movie_list,
                            name='Harry')#busca tambien el archivo movies.html en template
 
+# JINJA TEMPLATES 2
+@app.route('/tables')
+def movies_plus():
+    movies_dict = {'autopsy of jane doe': 02.14,
+                   'neon demon': 3.20,
+                   'ghost in a shell': 1.50,
+                   'kong: skull island': 3.50,
+                   'john wick 2': 02.52,
+                   'spiderman - homecoming': 1.48}
+
+    return render_template('table_data.html',
+                           movies=movies_dict,
+                           name='Sally')
+
 
 
 if __name__ == '__main__':
